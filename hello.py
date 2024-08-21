5694eff42df97e280a920f1246c2fb4d92f6cdbd
hankav21 <hankav21@o2.pl>
2024-08-21 22:10:43 +0200

    templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -9,12 +9,12 @@ def home():
 
 @app.route("/login")
 def login():
-    return f"<h1>LOGIN</h1>"
+    return render_template("login.html")
 
 @app.route("/lista_ksiazek")
 def lista_ksiazek():
-    return f"<h1>lista ksiazek</h1>"
+    return render_template("lista_ksiazek.html")
 
 @app.route("/lista_ksiazek/<ksiazka>")
 def widok_ksiazki(ksiazka):
-    return f"<h1>{ksiazka}</h1>"
+    return render_template("ksiazka.html", ksiazka=ksiazka)
